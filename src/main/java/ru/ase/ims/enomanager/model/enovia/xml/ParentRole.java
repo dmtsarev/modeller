d4ee2f73e8fd82935ede2e65c7480703768caf1b
mrstranger <ilya_goltsov@list.ru>
2020-07-21 19:44:26 +0300

    add multiple roleRefs parsing

--- src/main/java/ru/ase/ims/enomanager/model/enovia/xml/ParentRole.java
+++ src/main/java/ru/ase/ims/enomanager/model/enovia/xml/ParentRole.java
@@ -2,19 +2,20 @@ package ru.ase.ims.enomanager.model.enovia.xml;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import java.util.List;
 
 public class ParentRole {
     @XmlElement(name = "roleRef")
-    private String roleRef;
+    private List<String> roleRef;
 
     @XmlAttribute(name = "count")
     private Integer size;
 
-    public String getRoleRef() {
+    public List<String> getRoleRef() {
         return roleRef;
     }
 
-    public void setRoleRef(String roleRef) {
+    public void setRoleRef(List<String> roleRef) {
         this.roleRef = roleRef;
     }
 
